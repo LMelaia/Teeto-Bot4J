41246665087742a28c29f2647e8df430be20c05a
Luke Melaia <lukemelaia.personal@gmail.com>
2018-08-14 08:01:19 +0200

    Initial commit.
    Added main class and basic gradle configuration.

---
+++ src/main/java/net/lmelaia/teeto/Main.java
@@ -0,0 +1,16 @@
+package net.lmelaia.teeto;
+
+/**
+ * Entry class.
+ */
+public class Main {
+
+    /**
+     * Main method.
+     *
+     * @param args program arguments.
+     */
+    public static void main(String[] args){
+
+    }
+}
